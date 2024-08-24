6e0695cf7fb85f60a2ceea468597b998780f547d
Pulkit Dadheech <pulkitdadheech83@gmail.com>
2024-08-24 17:58:49 +0530

    refactor home to make sidebar appear

--- src/Dashboard_Body/Home.jsx
+++ src/Dashboard_Body/Home.jsx
@@ -1,16 +1,34 @@
-import './Home.css';
-import WidgetDashboardCreator from "./WidgetDashboardCreator";
+import React, { useState } from 'react';
+import WidgetDashboardCreator from './WidgetDashboardCreator';
+import SidebarMainPage from "../sidebar/SidebarMainPage";
+import './Home.css'; // Assuming you have a CSS file for styling
 
-export default function Home(){
-    const data=[{
+const Home = () => {
+    const [isActive, setIsActive] = useState(false); // Sidebar visibility state
 
-    }, {
+    // Function to toggle the sidebar visibility
+    const toggleSidebar = () => {
+        setIsActive((prevIsActive) => !prevIsActive);
+    };
 
-    }]
     return (
-        <div className='home'>
-            <div className={'home-name'} >CNAPP Dashboard</div>
-            <WidgetDashboardCreator header={'CSPM Executive Dasboard'} widgetData={data}/>
+        <div className="home">
+            <div className="home-header">
+                <div className="home-name">CNAPP Dashboard</div>
+                {/* Add Widget button to toggle sidebar */}
+                <button onClick={toggleSidebar} className="add-widget-button">
+                    Add Widget
+                </button>
+            </div>
+
+            <div className="home-content">
+                <WidgetDashboardCreator toggleSiderbar={toggleSidebar} />
+                <div className={`sidebar ${isActive ? 'active' : ''}`}>
+                    <SidebarMainPage toggleSidebar={toggleSidebar}/>
+                </div>
+            </div>
         </div>
-    )
-}
+    );
+};
+
+export default Home;
